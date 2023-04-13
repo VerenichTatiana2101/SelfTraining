5e3869d52ead51500e3c48ccecd22594b34d5289
Verenich Tatiana <verenik91@gmail.com>
2023-04-13 18:27:25 +0300

    Задача6. метод из массива слов в строку

---
+++ test6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Sentence Smash
+// Write a function that takes an array of words and smashes 
+// them together into a sentence and returns the sentence. 
+// You can ignore any need to sanitize words or add punctuation, 
+// but you should add spaces between each word. 
+// Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+
+string[] stringArray = new string[] { "hello", "world", "this", "is", "great" };
+string myString = Smash(stringArray);
+Console.WriteLine(myString);
+
+string Smash(string[] words)
+{
+    string concatenatedString = String.Join(" ", words);
+    return concatenatedString;
+}
