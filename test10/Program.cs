b5bd8780eb084187b9d009006c76a98a288f6315
Verenich Tatiana <verenik91@gmail.com>
2023-04-13 19:47:12 +0300

    Задача10. Удаление первого и последнего символа строки

---
+++ test10/Program.cs
@@ -0,0 +1,16 @@
+﻿// It's pretty straightforward. 
+// Your goal is to create a function that removes the first and last characters of a string. 
+// You're given one parameter, the original string. 
+// You don't have to worry with strings with less than two characters.
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,";
+Console.WriteLine(text);
+string res = RemoveChar(text);
+Console.WriteLine(res);
+
+string RemoveChar(string s)
+{
+    s = s.Remove(0, 1);   
+    s = s.Remove(s.Length - 1, 1);
+    return s;
+}
