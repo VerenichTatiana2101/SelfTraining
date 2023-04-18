dec5c9267b5c2b081aa67ac2030c47d63ee2b4fe
Verenich Tatiana <verenik91@gmail.com>
2023-04-18 21:59:11 +0300

    задача11. методы подсчёта строк в массиве, метод печати, метод пропуска пустых строк

---
+++ test11/Program.cs
@@ -0,0 +1,62 @@
+﻿string[] stringArray = new string[] { "o", "wod", "this", "is", "great" };
+PrintArray(stringArray);
+Console.WriteLine();
+int size = CountSizeArray(stringArray);
+string[] newArray = new string[size];
+ShortStrings(stringArray, newArray);
+PrintArray(newArray);
+
+void ShortStrings(string[] array, string[] newarray)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarray[j] = array[i];
+            j++;
+        }
+    }
+}
+
+int CountSizeArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            count++;
+    }
+    return count;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+// string[] Smash(string[] words)
+// {
+//     int j = 0;
+//     string[] resultString = new string[words.Length];
+//     for (int i = 0; i < words.Length; i++)
+//     {
+//         if (words[i].Length <= 3)
+//         {
+//             resultString[j] = words[i];
+//             j++;
+//         } 
+//     }
+//     Array.Resize(ref resultString, j);
+//     return resultString;
+// }
+
+
+
+
